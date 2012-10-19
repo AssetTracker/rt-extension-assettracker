40db031ee44f9d980198c7829a979d5556686bde
Thomas Sibley <trs@bestpractical.com>
2012-10-18 17:38:00 -0700

    Refactor processing %ARGS for new links during record creation
    The inputs processed are generated by /Elements/EditLinks and may be
    used by object types other than tickets.  Refactoring into a function
    lets other objects re-use the same code.
    
        (cherry-picked from rt commit 76a9d8dc934c014c86279b1c3b716a9b40609df7)

--- lib/RT/Interface/Web_ATOverlay.pm
+++ lib/RT/Interface/Web_ATOverlay.pm
@@ -457,18 +457,9 @@ sub CreateAsset {
         ContextObject   => $Type,
     );
 
-    # turn new link lists into arrays, and pass in the proper arguments
-    foreach my $type ( keys %RT::Link::DIRMAP ) {
-        for ([Base => "new-$type"], [Target => "$type-new"]) {
-            my ($direction, $key) = @$_;
-            next unless $ARGS{$key};
-            $create_args{ $RT::Link::DIRMAP{$type}->{$direction} } = [
-                grep $_, split ' ', $ARGS{$key}
-            ];
-        }
-    }
+    my %links = ProcessLinksForCreate( ARGSRef => \%ARGS );
 
-    my ( $id, $Trans, $ErrMsg ) = $Asset->Create(%create_args, %cfs);
+    my ( $id, $Trans, $ErrMsg ) = $Asset->Create(%create_args, %links, %cfs);
     unless ( $id && $Trans ) {
         Abort($ErrMsg);
     }
