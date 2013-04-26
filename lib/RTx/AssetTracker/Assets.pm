fd912c0e353cd256a7744f92338c7af972e4fd30
Alex Vandiver <alexmv@bestpractical.com>
2013-04-26 19:06:28 -0400

    Remove uses of ::Unique; our SELECT DISTINCT is sufficient
    Rather than import a mixin (twice, in Tickets' case (?!)) which
    unique-ifies the results, rely on our SELECT DISCINCT machinery.  The
    original code was added when DistinctQuery was unimplemented for some
    database handles.
    
    Cherry-picked from bcd79f3afc85d8976a65b94c5de9c158b6d1962f on
    4.2-trunk.  It is necessary because the ::Unique mixin provides an
    (unnecessary) AddRecord method, which the following commits would
    conflict with.
    
        (Cherry picked from RT commit 0e55210e0dc5e09e9863fc725145c34e5291cacd)

--- lib/RTx/AssetTracker/Assets.pm
+++ lib/RTx/AssetTracker/Assets.pm
@@ -1807,16 +1807,6 @@ sub OrderByCols {
                QUOTEVALUE => 1,
                ENTRYAGGREGATOR => 'AND',
            ) if $CFs;
-           unless ($cf_obj) {
-               # For those cases where we are doing a join against the
-               # CF name, and don't have a CFid, use Unique to make sure
-               # we don't show duplicate assets.  NOTE: I'm pretty sure
-               # this will stay mixed in for the life of the
-               # class/package, and not just for the life of the object.
-               # Potential performance issue.
-               require DBIx::SearchBuilder::Unique;
-               DBIx::SearchBuilder::Unique->import;
-           }
            my $CFvs = $self->Join(
                TYPE   => 'LEFT',
                ALIAS1 => $ObjectCFs,
