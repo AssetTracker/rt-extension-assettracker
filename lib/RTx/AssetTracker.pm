d3a20b3a6ee056113dc98bd0f518f0050c551077
Bradley Bell <bradleyb@uw.edu>
2013-05-04 22:02:07 -0700

    remove obsolete LoadConfig

--- lib/RTx/AssetTracker.pm
+++ lib/RTx/AssetTracker.pm
@@ -26,49 +26,8 @@ use RTx::AssetTracker::Type;
 use RTx::AssetTracker::Asset;
 use RTx::AssetTracker::Templates;
 use RTx::AssetTracker::Scrips;
-use RTx::AssetTracker::Scrips;
 use RTx::AssetTracker::ScripConditions;
 use RTx::AssetTracker::ScripActions;
-use RT::Shredder;
-
-use vars qw(
-    $CORE_CONFIG_FILE
-    $SITE_CONFIG_FILE
-    $EtcPath
-    $VarPath
-    $LocalPath
-    $LocalEtcPath
-    $LocalLexiconPath
-    $ATConfigDone
-);
-
-$LocalEtcPath = "$RT::LocalPluginPath/RTx-AssetTracker/etc";
-$CORE_CONFIG_FILE = "$LocalEtcPath/AT_Config.pm";
-$SITE_CONFIG_FILE = "$LocalEtcPath/AT_SiteConfig.pm";
-$ATConfigDone = 0;
-
-=head2 LoadConfig
-
-Load RT's config file. First, go after the core config file.
-After that, go after the site config.
-
-=cut
-
-sub LoadConfig {
-    #local *Set = sub { $_[0] = $_[1] unless defined $_[0] };
-    #if ( -f "$SITE_CONFIG_FILE" ) {
-        #require $SITE_CONFIG_FILE
-          #|| die ("Couldn't load AT config file  '$SITE_CONFIG_FILE'\n$@");
-    #}
-    #require $CORE_CONFIG_FILE
-      #|| die ("Couldn't load AT config file '$CORE_CONFIG_FILE'\n$@");
-#
-    #RT->Init();
-    RTx::AssetTracker::Type->ConfigureRoles();
-    RTx::AssetTracker::Asset->ConfigureLinks();
-    $ATConfigDone = 1;
-}
-
 
 # Create a system object for AssetTracker
 $RTx::AssetTracker::System = RTx::AssetTracker::System->new($RT::SystemUser);
