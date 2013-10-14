vandaemb/rt

process stage when we apply scrips to queues
now stage is property of a link between queue and scrip

lib/RT/Scrip.pm
@@ -239,10 +239,10 @@ sub NotAddedTo {
 
 sub AddToObject {
     my $self = shift;
-    my $object = shift;
+    my %args = @_%2? (ObjectId => @_) : (@_);
 
     my $rec = RT::ObjectScrip->new( $self->CurrentUser );
-    return $rec->Apply( Scrip => $self, ObjectId => $object );
+    return $rec->Apply( %args, Scrip => $self );
 }
 
 sub RemoveFromObject {

AssetTracker/rt-extension-assettracker

    (cherry-picked from rt commit 591f2c5b891b367f23b963a5b7401da6c62bf84e)

lib/RTx/AssetTracker/Scrip.pm
@@ -241,10 +241,10 @@ sub NotAddedTo {
 
 sub AddToObject {
     my $self = shift;
-    my $object = shift;
+    my %args = @_%2? (ObjectId => @_) : (@_);
 
     my $rec = RTx::AssetTracker::ObjectScrip->new( $self->CurrentUser );
-    return $rec->Add( Scrip => $self, ObjectId => $object );
+    return $rec->Add( %args, Scrip => $self );
 }
 
 sub RemoveFromObject {