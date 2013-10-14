a1eb3e1d7b26415f97371a2823b41f98534291e5
Bradley Bell <bradleyb@uw.edu>
2013-10-13 19:23:22 -0700

    update shredder methods

--- lib/RTx/AssetTracker/Template.pm
+++ lib/RTx/AssetTracker/Template.pm
@@ -958,6 +958,41 @@ sub _CoreAccessible {
  }
 };
 
+
+### Shredder methods ###
+
+use RT::Shredder::Constants;
+use RT::Shredder::Exceptions;
+use RT::Shredder::Dependencies;
+
+
+sub __DependsOn
+{
+    my $self = shift;
+    my %args = (
+            Shredder => undef,
+            Dependencies => undef,
+            @_,
+           );
+    my $deps = $args{'Dependencies'};
+    my $list = [];
+
+# Scrips
+    my $objs = RTx::AssetTracker::Scrips->new( $self->CurrentUser );
+    $objs->Limit( FIELD => 'Template', VALUE => $self->Id );
+    push( @$list, $objs );
+
+    $deps->_PushDependencies(
+        BaseObject => $self,
+        Flags => DEPENDS_ON,
+        TargetObjects => $list,
+        Shredder => $args{'Shredder'},
+    );
+
+    return $self->SUPER::__DependsOn( %args );
+}
+
+
 RT::Base->_ImportOverlays();
 
 1;
