petrkle/rt

make sure we pass id further, not a string
If caller passes in queue name then we don't complain,
but later it's INTifyed to zero. We shouldn't let
this happen.

lib/RT/Scrip.pm
@@ -251,6 +251,8 @@ sub AddToObject {
         $queue->Load( $args{'ObjectId'} );
         return (0, $self->loc('Invalid queue'))
             unless $queue->id;
+
+        $args{'ObjectId'} = $queue->id;
     }
     return ( 0, $self->loc('Permission Denied') )
         unless $self->CurrentUser->PrincipalObj->HasRight(

AssetTracker/rt-extension-assettracker

    (cherry-picked from rt commit 74f399be6f0386a504fc7b0e267df29ae9242f75)

lib/RTx/AssetTracker/Scrip.pm
@@ -245,10 +245,8 @@ sub AddToObject {
 
     my $assettype;
     if ( $args{'ObjectId'} ) {
-        $assettype = RTx::AssetTracker::Type->new( $self->CurrentUser );
-        $assettype->Load( $args{'ObjectId'} );
-        return (0, $self->loc('Invalid asset type'))
-            unless $assettype->id;
+
+        $args{'ObjectId'} = $assettype->id;
     }
     return ( 0, $self->loc('Permission Denied') )
         unless $self->CurrentUser->PrincipalObj->HasRight(