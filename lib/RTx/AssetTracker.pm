a0ae6e4ade8777b68c46af8a50194f3f451fc514
Kevin Falcone <falcone@bestpractical.com>
2014-01-06 18:23:40 -0500

    Update the copyright for 2014 as we get ready for new releases
        (Cherry picked from RT commit 7e24399a7d0139e6da1544a4f8f60a2ac7310192)

--- lib/RTx/AssetTracker.pm
+++ lib/RTx/AssetTracker.pm
@@ -1,7 +1,5 @@
 # BEGIN LICENSE BLOCK
 # 
-#  Copyright (c) 2002-2003 Jesse Vincent <jesse@bestpractical.com>
-#  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of version 2 of the GNU General Public License 
 #  as published by the Free Software Foundation.
